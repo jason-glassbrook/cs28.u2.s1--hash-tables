f40495a0ed1b49749f94bf0d4ceb14553f665cd4
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-05-08 12:39:21 -0400

    tools.math_tools: don't filter Nones -- they actually indicate an error in config

--- tools/math_tools.py
+++ tools/math_tools.py
@@ -3,9 +3,9 @@
 
 def int_min(*args, **kwargs):
 
-    return int(min(filter(None, args), **kwargs))
+    return int(min(args, **kwargs))
 
 
 def int_max(*args, **kwargs):
 
-    return int(max(filter(None, args), **kwargs))
+    return int(min(args, **kwargs))
